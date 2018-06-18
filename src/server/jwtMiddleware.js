d7b2f960a8a93618cbfb51f97939e69f4c6599a9
johnnys martins <johnnys.rdsm@mail.com>
2018-06-18 11:14:34 -0300

    efetuando algumas mudancas no middleware do jwt

--- src/server/jwtMiddleware.js
+++ src/server/jwtMiddleware.js
@@ -1,18 +1,18 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
 const jwtMiddleware = ({ exclusions }) => {
 	return async (req, res, next) => {
 		if (!exclusions.includes(req.href())) {
-			const token = req.headers['x-access-token'];
+			const token = req.headers['x-access-token']
 			if (!token) {
 				res.json(403, { error: 'Token não fornecido' })
 				return
 			}
 
 			try {
-				req.decoded = jwt.verify(token, process.env.JWT_SECRET);
-			} catch (error) {
-				res.json(403, { error: 'falha ao autenticar o token' });
+				req.decoded = jwt.verify(token, process.env.JWT_SECRET)
+			} catch (err) {
+				res.json(403, { error: 'falha ao autenticar o token', err})
 				return
 			}
 		}
